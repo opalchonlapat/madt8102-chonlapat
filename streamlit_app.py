5d8d78e4fdb5c5b24825376d80fce7dfacfe22e9
Chonlapat Patanajirasit <30840253+opalchonlapat@users.noreply.github.com>
2024-09-28 04:41:16 +0000

    First init

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,37 @@
-import streamlit as st
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain_core.prompts import ChatPromptTemplate
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+import streamlit as st
+import os
+
+with st.sidebar:
+    gemini_api_key = st.text_input("Gemini API Key: ", 
+                               placeholder="Type your API Key here...", 
+                               type="password")
+    if "GOOGLE_API_KEY" not in os.environ:
+        os.environ["GOOGLE_API_KEY"] = gemini_api_key
+    llm = ChatGoogleGenerativeAI(
+            model="gemini-pro",
+            temperature=1
+        )
+
+st.title("🤖 My chatbot app")
+
+if "messages" not in st.session_state:
+    st.session_state["messages"] = []
+
+for msg in st.session_state.messages:
+    st.chat_message(msg["role"]).write(msg["content"])
+
+user_input = st.chat_input()
+if user_input:
+    if not gemini_api_key:
+        st.info("Please add your Gemini API key to continue.")
+        st.stop()
+
+    st.session_state.messages.append({"role": "user", "content": user_input})
+    st.chat_message("user").write(user_input)
+    response = llm.invoke(st.session_state["messages"])
+    bot_response = response.content
+    st.session_state.messages.append({"role": "assistant", "content": bot_response})
+    st.chat_message("assistant").write(bot_response)
